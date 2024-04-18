3ac32741b918fbf729d988d127841710d1172b3f
Your Name <you@example.com>
2024-04-18 14:02:54 +0530

    available meals

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () =>{
+    return <section className={classes.summary}>
+        <h2>Delicious Food, Delivered too you</h2>
+        <p>
+            Choose yoyr ugbh  uv f f vuf vf 
+        </p>
+    </section>
+}
+
+export default MealsSummary;
