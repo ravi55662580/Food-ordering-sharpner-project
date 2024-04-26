67a2e7e154514046e8fa5fe6a64df24fb216e174
Your Name <you@example.com>
2024-04-26 15:35:42 +0530

    Cart Count

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 
-import {Fragment} from 'react';
+import {useState} from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart'
+import CartProvider from './Store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown]= useState(false);
+  const showCartHandler = () =>{
+    setCartIsShown(true);
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
   return (
-    <Fragment>
-      <Header/>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShownCart ={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
